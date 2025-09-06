a32de7a5c342485d1ee19ae5a5ccd1d1fed50dd5
hariswara <hariswaranvenkatesh@gmail.com>
2025-09-06 21:10:44 +0530

    feat: set the routing with font primary colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetials from "./pages/MovieDetials";
+import SeatLayout from "./pages/SeatLayout";
+import MyBookings from "./pages/MyBookings";
+import Favorite from "./pages/Favorite";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div className="">Hello every one</div>;
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
+  return (
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetials />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favorite" element={<Favorite />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  );
 };
 
 export default App;
---
+++ src/pages/MovieDetials.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovieDetials = () => {
+  return <div></div>;
+};
+
+export default MovieDetials;
